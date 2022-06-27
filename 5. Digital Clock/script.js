b21034d777bfc47e6dba7ce84dec9c1807a5e7b3
taimarahman <taimarh@gmail.com>
2022-06-27 23:03:11 +0600

    day 5- updated

--- 5. Digital Clock/script.js
+++ 5. Digital Clock/script.js
@@ -1,32 +1,32 @@
 function convertToTwoDigits(digit) {
-    return (digit < 10) ? '0' + digit : digit;
+    return (digit < 10) ? '0'+ digit : digit;
+    // return digit;
 }
 
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
-const ampm = document.getElementById('ampm');
+const am_pm = document.getElementById('am-pm');
 
 setInterval(()=> {
 
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-    let am = h >= 12 ? "PM" : "AM";
+    let ap= h >= 12 ? "PM" : "AM";
     
     if(h>12) {
         h-=12;
     }
+    else if(h==0) {
+        h = 12;
+    }
     
-    convertToTwoDigits(h);
-    convertToTwoDigits(m);
-    convertToTwoDigits(s);
-    
+    hours.innerHTML = convertToTwoDigits(h);
+    minutes.innerHTML = convertToTwoDigits(m);
+    seconds.innerHTML = convertToTwoDigits(s);
+    am_pm.innerHTML = ap;
     
 });
 
 
-hours.innerHTML = h;
-minutes.innerHTML = m;
-seconds.innerHTML = s;
-ampm.innerHTML = am;
