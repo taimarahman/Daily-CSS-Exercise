89541606c2421c0a5705358490e7e05eeb0be778
taimarahman <taimarh@gmail.com>
2022-06-29 23:28:56 +0600

    day 5- updated

--- 5. Digital Clock/script.js
+++ 5. Digital Clock/script.js
@@ -1,12 +1,23 @@
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const am_pm = document.getElementById('am-pm');
+
+const hh = document.getElementById('hh');
+const mm = document.getElementById('mm');
+const ss = document.getElementById('ss');
+
 function convertToTwoDigits(digit) {
     return (digit < 10) ? '0'+ digit : digit;
     // return digit;
 }
 
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
-const am_pm = document.getElementById('am-pm');
+function setDashOffset(mul, div) {
+    const dashOffset = 440 - (440 * mul) / div;
+    return dashOffset;
+}
+
+
 
 setInterval(()=> {
 
@@ -26,6 +37,10 @@ setInterval(()=> {
     minutes.innerHTML = convertToTwoDigits(m);
     seconds.innerHTML = convertToTwoDigits(s);
     am_pm.innerHTML = ap;
+
+    hh.style.strokeDashoffset = setDashOffset(h,12);
+    mm.style.strokeDashoffset = setDashOffset(m,60);
+    ss.style.strokeDashoffset = setDashOffset(s,60);
     
 });
 
